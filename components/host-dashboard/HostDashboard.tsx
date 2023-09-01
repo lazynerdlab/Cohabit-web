2ad9174ef9e7ad46646487c67be0fc8968683dd9
roscoericch <sikirurazak1@gmail.com>
2023-09-01 22:27:31 +0100

    updated charts responsiveness

--- components/host-dashboard/HostDashboard.tsx
+++ components/host-dashboard/HostDashboard.tsx
@@ -14,22 +14,26 @@ const HostDashboard = () => {
         <h5 className="text-[#25324B] mb-[0.5rem] text-[14px] md:text-[18px] font-bold leading-[28.80px]">
           Here is your apartment list statistics report
         </h5>
-        <div className="w[98%] py-[1rem] px[2rem] gap-[0.5rem] grid grid-cols-1 md:grid-cols-3 items-center justify-between">
-          <div className="bg-[#010886] flex items-center justify-between gap-[0.3rem] text-[48px] font-[700] text-[#FFF] p-[15px]">
+        <div className="w[98%]  py-[0.5rem] md:py-[1rem] px[2rem] gap-[0.5rem] grid grid-cols-1 md:grid-cols-3 items-center justify-between">
+          <div className="bg-[#010886] flex items-center justify-between gap-[0.3rem] text-[20px] md:text-[48px] font-[700] text-[#FFF] p-[15px]">
             <span>76</span>
-            <span className="text-[18px] font-[400]">View your apartment</span>
+            <span className="text-[12px] md:text-[18px] font-[400]">
+              View your apartment
+            </span>
             <ShortArrow className="stroke-[#FFF]" />
           </div>
           <div className="bg-[#16DD97] flex items-center justify-between gap-[0.3rem] text-[48px] font-[700] text-[#000] p-[15px]">
-            <span className="text-[48px]">3</span>
-            <span className="text-[18px] font-[400]">
+            <span className="text-[20px] md:text-[48px]">3</span>
+            <span className="text-[12px] md:text-[18px] font-[400]">
               Schedule for visiting
             </span>
             <ShortArrow className="stroke-[#000]" />
           </div>
-          <div className="bg-[#9CA9F9] flex items-center justify-between gap-[0.3rem] text-[48px] font-[700] text-[#000] p-[15px]">
+          <div className="bg-[#9CA9F9] flex items-center justify-between gap-[0.3rem] text-[20px] md:text-[48px] font-[700] text-[#000] p-[15px]">
             <span>24</span>
-            <span className="text-[18px] font-[400]">Messages received</span>
+            <span className="text-[12px] md:text-[18px] font-[400]">
+              Messages received
+            </span>
             <ShortArrow className="stroke-[#000]" />
           </div>
         </div>
