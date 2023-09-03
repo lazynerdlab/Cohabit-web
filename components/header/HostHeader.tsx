9cb294bc3f72bd56cc251535cce4c8a7f7bdfe75
roscoericch <sikirurazak1@gmail.com>
2023-09-03 09:21:30 +0100

    updated host header

--- components/header/HostHeader.tsx
+++ components/header/HostHeader.tsx
@@ -4,11 +4,12 @@ import { CustomButton as Button } from "@/lib/AntDesignComponents";
 import { useRouter } from "next/navigation";
 import Notification from "@/assets/icons/Notification";
 import PlusIcon from "@/assets/icons/PlusIcon";
+import HomeIconsm from "@/assets/icons/HomeIconsm";
 
 const HostHeader = ({ children }: HeaderType) => {
   const { replace } = useRouter();
   return (
-    <div className="grid grid-cols-[40%_20%_40%] md:grid-cols-[70%_10%_20%] items-center px-[1rem] sticky shadow-inner shadow-[#D6DDEB]">
+    <div className="grid grid-cols-[50%_20%_30%] md:grid-cols-[60%_20%_20%] lg:grid-cols-[70%_10%_20%] items-center px-[0.5rem] lg:px-[1rem] sticky shadow-inner shadow-[#D6DDEB]">
       {children}
       <div className="flex justify-end">
         <Notification />
@@ -20,8 +21,9 @@ const HostHeader = ({ children }: HeaderType) => {
           type="primary"
         >
           <div className="flex items-center justify-center gap-[0.2rem]">
-            <PlusIcon className="fill-[#FFF] hidden md:block" />
-            <p className="text-[12px] md:text-[14px] font-[600]">
+            <PlusIcon className="fill-[#FFF]" />
+            <HomeIconsm className="stroke-[#FFF] md:hidden" />
+            <p className="text-[12px] md:text-[14px] font-[600] hidden md:block">
               Post an apartment
             </p>
           </div>
