afa0eb79fe43a04c8d54269c47abc429293501ea
Chukwuemeka Nkwo <emekankwo49@gmail.com>
2023-09-27 23:38:29 +0100

    updated styling

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,7 +5,8 @@ import Registry from "./registry";
 const Page = () => {
   return (
     <Registry>
-      <div className="grid grid-cols-1 grid-rows-[10%_90%] max-h-screen overflow-y-scroll">
+      <div className="grid grid-cols-1 grid-rows-[10%_90%] max-h-screen 3xl:max-w-[1600px] 3xl:mx-auto 3
+        xl:shadow-md">
         <Header>
           <h4 className="text-[#25324B] text-[18px] md:text-[25px] font-[700]">
             Dashboard
