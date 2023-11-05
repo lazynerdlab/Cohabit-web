e3a12a310ffa2522ec64323edb850733d33519ac
roscoericch <sikirurazak1@gmail.com>
2023-11-05 02:19:31 +0100

    update respnsiveness

--- components/find-property/FindProperty.tsx
+++ components/find-property/FindProperty.tsx
@@ -6,6 +6,7 @@ import HomeCard from "../cards/HomeCard";
 import Filter from "./Filter";
 import {
   CustomPagination as Pagination,
+  CustomSelect as Select,
 } from "@/lib/AntDesignComponents";
 
 import SearchIcon from "@/assets/icons/SearchIcon";
@@ -70,8 +71,12 @@ const FindProperty = () => {
 
       if (values) {
         const filteredValues = Array.isArray(values)
-          ? values.map((value: Record<string, any>) => value.value || value.id).filter(Boolean)
-          : [values].map((value: Record<string, any>) => value.value || value.id).filter(Boolean);
+          ? values
+              .map((value: Record<string, any>) => value.value || value.id)
+              .filter(Boolean)
+          : [values]
+              .map((value: Record<string, any>) => value.value || value.id)
+              .filter(Boolean);
 
         if (filteredValues.length > 0) {
           const encodedValues = filteredValues
@@ -98,64 +103,120 @@ const FindProperty = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchValue, page, budgets, propertyType, locations, areas]);
   return (
-    <div className="grid grid-cols-[5%_95%] mini:grid-cols-[25%_75%] max-h-screen min-h-screen">
-      <div>
-        {Number(width) < 700 &&
-          (filter ? (
-            <SearchIcon
-              onClick={() => {
-                setFilter((prev) => !prev);
-              }}
-              className="cursor-pointer stroke-colorPrimary"
-            />
-          ) : (
-            <FiFilter
-              className="cursor-pointer"
-              onClick={() => {
-                setFilter((prev) => !prev);
-              }}
-            />
-          ))}
-        {filter && <Filter locations={locations} setLocations={setLocations} areas={areas} setAreas={setAreas} propertyTypes={propertyType} setPropertyType={setPropertyType} budgets={budgets} setBudgets={setBudgets} />}
-      </div>
-      <div className="flex flex-col gap-3">
-        <div className="grid grid-cols-[40%_60%] md:grid-cols-[45%_50%_5%] w-[98%] mx-auto">
-          <div className="text-[#25324B] flex flex-col gap-[0.1rem]">
-            <h4 className="lg:text-[24px] text-[12px] md:text-[18px] font-[700]">
-              All Property
-            </h4>
-            <p className="text-[10px] md:text-[16px] font-[400]">
-              Showing {data?.meta?.total}  results
-            </p>
+    <div className="drawer relative min-h-screen max-h-screen overflow-hidden">
+      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content overflow-y-scroll bg-[#EEF2F7]">
+        <div className="grid grid-cols-1 gap-[0.5rem] overflow-y-scroll">
+          <div className="grid grid-cols-[40%_60%] md:grid-cols-[45%_50%_5%] items-center w-[98%] mx-auto">
+            <div className="flex items-center">
+              <label htmlFor="my-drawer-3" className="flex text-blue-800 px-3">
+                <FiFilter className="cursor-pointer" />
+              </label>
+              <div className="text-[#25324B] flex flex-col gap-[0.1rem]">
+                <h4 className="lg:text-[24px] text-[12px] md:text-[18px] font-[700]">
+                  All Property
+                </h4>
+              </div>
+              <p className="text-[10px] md:text-[16px] font-[400]">
+                -Showing {data?.meta?.total} results
+              </p>
+            </div>
+            {/* <div className="flex items-center gap-[0.1rem] w-full">
+              <p className="text-[#7C8493] text-[10px] md:text-[16px] font-[400]">
+                Sort by:
+              </p>
+              <Select options={options} defaultValue={"Most Relevant"} />
+            </div>
+            <SortIcon className="hidden md:block" /> */}
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-[1rem] w-[98%] mx-auto">
+            {loading ? (
+              <> Loading...</>
+            ) : (
+              <>
+                {" "}
+                {houseData?.map(
+                  (item: Record<string, any>, i: number | string) => (
+                    <HomeCard key={i} data={item} />
+                  )
+                )}
+              </>
+            )}
+          </div>
+          <div className="w-full flex justify-center">
+            <Pagination total={50} />
           </div>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-[1rem] w-[98%] mx-auto">
-          {
-            loading ? (
-              <> Loading...</>
-            ) :
-              <> {houseData?.map((item: Record<string, any>, i: number | string) => (
-                <HomeCard key={i} data={item} />
-              ))}</>
-
-          }
-
-        </div>
-        <div className="w-full flex justify-center mt-4">
-          <Pagination
-            pageSize={rowsPerPage}
-            total={data?.meta?.total}
-            current={data?.meta?.current_page}
-            onChange={(page, count) => {
-              setRowsPerPage(count || 10);
-              setPage(page);
-            }}
-          />
-
-        </div>
       </div>
-      {/* <Filter /> */}
+      <Filter
+        locations={locations}
+        setLocations={setLocations}
+        areas={areas}
+        setAreas={setAreas}
+        propertyTypes={propertyType}
+        setPropertyType={setPropertyType}
+        budgets={budgets}
+        setBudgets={setBudgets}
+      />
     </div>
+    // <div className="grid grid-cols-[5%_95%] mini:grid-cols-[25%_75%] max-h-screen min-h-screen">
+    //   <div>
+    //     {Number(width) < 700 &&
+    //       (filter ? (
+    //         <SearchIcon
+    //           onClick={() => {
+    //             setFilter((prev) => !prev);
+    //           }}
+    //           className="cursor-pointer stroke-colorPrimary"
+    //         />
+    //       ) : (
+    //         <FiFilter
+    //           className="cursor-pointer"
+    //           onClick={() => {
+    //             setFilter((prev) => !prev);
+    //           }}
+    //         />
+    //       ))}
+    //     {filter && <Filter locations={locations} setLocations={setLocations} areas={areas} setAreas={setAreas} propertyTypes={propertyType} setPropertyType={setPropertyType} budgets={budgets} setBudgets={setBudgets} />}
+    //   </div>
+    //   <div className="flex flex-col gap-3">
+    //     <div className="grid grid-cols-[40%_60%] md:grid-cols-[45%_50%_5%] w-[98%] mx-auto">
+    //       <div className="text-[#25324B] flex flex-col gap-[0.1rem]">
+    //         <h4 className="lg:text-[24px] text-[12px] md:text-[18px] font-[700]">
+    //           All Property
+    //         </h4>
+    //         <p className="text-[10px] md:text-[16px] font-[400]">
+    //           Showing {data?.meta?.total}  results
+    //         </p>
+    //       </div>
+    //     </div>
+    //     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-[1rem] w-[98%] mx-auto">
+    // {
+    //   loading ? (
+    //     <> Loading...</>
+    //   ) :
+    //     <> {houseData?.map((item: Record<string, any>, i: number | string) => (
+    //       <HomeCard key={i} data={item} />
+    //     ))}</>
+
+    // }
+
+    //     </div>
+    //     <div className="w-full flex justify-center mt-4">
+    //       <Pagination
+    //         pageSize={rowsPerPage}
+    //         total={data?.meta?.total}
+    //         current={data?.meta?.current_page}
+    //         onChange={(page, count) => {
+    //           setRowsPerPage(count || 10);
+    //           setPage(page);
+    //         }}
+    //       />
+
+    //     </div>
+    //   </div>
+    //   {/* <Filter /> */}
+    // </div>
     // <div className="grid grid-cols-[5%_95%] mini:grid-cols-[25%_75%] max-h-screen min-h-screen">
     //   <div>
     //     {/* {Number(width) < 700 &&
